5aeb7fd4fc8f5b89834f1ac1e47473f66e36c351
Denis Baranov <nazguldragon@mail.ru>
2019-03-13 16:23:52 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 /**
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
- */
+ */ 
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+var number = 0;
+  for (i=0;i<preferences.length;i=i+1) {
+var second = preferences[preferences[i]-1];
+var third = preferences[second-1];
+if ((i+1) !=preferences[i] && second!=third) {//чтобы не считало треугольником число со своим же номером
+if ((i+1) == third) {
+number = number+1;
+}
+}; 
+  };
+  return number/3;//деление на 3, т.к. находит каждый из треугольник по одной из вершин, т.е. три раза
+
+  }
